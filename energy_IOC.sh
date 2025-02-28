157a835e52401d7d546d9bd47a80f26558d404f7
Francesco De Carlo <decarlof@gmail.com>
2025-02-27 18:59:20 -0600

    tested ok

--- energy_IOC.sh
+++ energy_IOC.sh
@@ -1,7 +1,19 @@
-gnome-terminal --tab --title "energy IOC" -- bash -c "ssh -t 2bmb@tomdet \
-'bash ~/bin/kill_IOC.sh energyApp;  \
-bash ~/bin/kill_server.sh start_energy.py; \
-cd /home/beams/2BMB/epics/synApps/support/energy/iocBoot/iocEnergy_2BM/; \
-./start_IOC;\
-bash'" 
+#!/bin/bash
+
+# Define variables
+TAB_NAME="energy IOC"
+REMOTE_USER="2bmb"
+REMOTE_HOST="tomdet"
+SCRIPT_NAME="start_energy.py"
+APP_NAME="energyApp"
+WORK_DIR="/home/beams/2BMB/epics/synApps/support/energy/iocBoot/iocEnergy_2BM/"
+
+# Open a new tab in gnome-terminal, SSH into tomdet, activate conda, and run Python (without login shell)
+gnome-terminal --tab --title="$TAB_NAME" -- bash -c "
+    ssh -t ${REMOTE_USER}@${REMOTE_HOST} '
+        kill_IOC.sh ${APP_NAME}
+        cd ${WORK_DIR}
+        ./start_IOC;
+    ';
+"
 
--- energy_server.sh
+++ energy_server.sh
@@ -1,5 +1,20 @@
-gnome-terminal --tab --title "energy py server" -- bash -c "ssh -t 2bmb@tomdet \
-'bash ~/bin/kill_server.sh start_energy.py;  \
-cd /home/beams/2BMB/epics/synApps/support/energy/iocBoot/iocEnergy_2BM/; \
-bash -c \"source ~/.bashrc; conda activate ops; python -i start_energy.py\";\
-bash'" 
+#!/bin/bash
+
+# Define variables
+TAB_NAME="energy py server"
+REMOTE_USER="2bmb"
+REMOTE_HOST="tomdet"
+CONDA_ENV="ops"
+SCRIPT_NAME="start_energy.py"
+WORK_DIR="/home/beams/2BMB/epics/synApps/support/energy/iocBoot/iocEnergy_2BM/"
+
+# Open a new tab in gnome-terminal, SSH into tomdet, activate conda, and run Python (without login shell)
+gnome-terminal --tab --title="$TAB_NAME" -- bash -c "
+    ssh -t ${REMOTE_USER}@${REMOTE_HOST} '
+        cd ${WORK_DIR}
+        conda activate ${CONDA_ENV}
+        kill_server.sh ${SCRIPT_NAME}
+        python -i ${SCRIPT_NAME}
+    ';
+"
+
