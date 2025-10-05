c6b804034523be3fcfdd89b858acf2ce611f9348
Francesco De Carlo <decarlof@gmail.com>
2025-10-05 15:01:56 -0500

    first commit

---
+++ tomostream_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define variables
+TAB_NAME="tomoStream py server"
+REMOTE_USER="2bmb"
+REMOTE_HOST="tomo1"
+CONDA_ENV="tomostream"
+SCRIPT_NAME="start_tomostream.py"
+WORK_DIR="/home/beams/2BMB/epics/synApps/support/tomostream/iocBoot/iocTomoStream_2BM/"
+
+# Open a new tab in gnome-terminal, SSH into tomdet, activate conda, and run Python (without login shell)
+gnome-terminal --tab --title="$TAB_NAME" -- bash -c "
+    ssh -t ${REMOTE_USER}@${REMOTE_HOST} '
+        cd ${WORK_DIR}
+        conda activate ${CONDA_ENV}
+        kill_server.sh ${SCRIPT_NAME}
+        python -i ${SCRIPT_NAME}
+    ';
+"
+
+
---
+++ tomostream_server_stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define variables
+TAB_NAME="tomoStream py server"
+REMOTE_USER="2bmb"
+REMOTE_HOST="tomo1"
+SCRIPT_NAME="start_tomostream.py"
+
+# Open a new tab in gnome-terminal, SSH into tomdet, activate conda, and run Python (without login shell)
+gnome-terminal --tab --title="$TAB_NAME" -- bash -c "
+    ssh -t ${REMOTE_USER}@${REMOTE_HOST} '
+        kill_server.sh ${SCRIPT_NAME}
+    ';
+"
