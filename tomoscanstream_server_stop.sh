5f5e309191deda3193bd843c3c5ab6b377e532b1
Francesco De Carlo <decarlof@gmail.com>
2025-10-05 15:00:17 -0500

    first commit

--- tomoscanstream_server_stop.sh
+++ tomoscanstream_server_stop.sh
@@ -1,4 +1,14 @@
-gnome-terminal --tab --title "tomoScan py server" -- bash -c "ssh -t 2bmb@tomdet \
-'bash ~/scripts/kill_server.sh start_tomoscan.py;  \
-bash'" 
+#!/bin/bash
 
+# Define variables
+TAB_NAME="tomoScanStream py server"
+REMOTE_USER="2bmb"
+REMOTE_HOST="tomdet"
+SCRIPT_NAME="start_tomoscan.py"
+
+# Open a new tab in gnome-terminal, SSH into tomdet, activate conda, and run Python (without login shell)
+gnome-terminal --tab --title="$TAB_NAME" -- bash -c "
+    ssh -t ${REMOTE_USER}@${REMOTE_HOST} '
+        kill_server.sh ${SCRIPT_NAME}
+    ';
+"
